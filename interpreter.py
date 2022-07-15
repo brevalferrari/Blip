255b4c59ed41f553f5898ebb81ee145ab0310990
BrevalF <breval.ferrari@gmail.com>
2022-07-15 11:46:14 +0200

    Better spot function, sections identified

--- interpreter.py
+++ interpreter.py
@@ -6,13 +6,18 @@ def dico(file):
     for k in ['notes','&also','instruments']:
         if k in d:d[k]=d[k].split(',')
     return d
-def spot(possibilities,text):
+def spot(possibilities,text,canbedigit=False):
     ps=[d[k] for k in possibilities]
     ps.sort(key=len)
     ps.reverse()
     while ps:
         if ps[0]==text[:len(ps[0])]:return ps[0]
         ps.pop(0)
+    if canbedigit:
+        i=0
+        while text:
+            if not text[i].isdigit():return text[:i]
+            i+=1
 with open('example','rb') as f:
     F=f.read().decode('utf-8','replace').splitlines()
 d=dico(F.pop(0))# dict of found settings
@@ -24,9 +29,11 @@ m=False# block match with known keyword
 while F:
     if F[:b]==d['channel'].split('%')[0]:
         m=True
-        c=spot(['globalsection','drumsection'],F[b:])
+        c=spot(['globalsection','drumsection'],F[b:],True)
+        b+=len(c)
         if c==d['globalsection']:print('global')
         elif c==d['drumsection']:print('drum')
-        else:print('channel')
+        else:print(f'channel {c}')
     F=F[b if m else 1:]
+    b=len(d['channel'].split('%')[0])
     m=False
